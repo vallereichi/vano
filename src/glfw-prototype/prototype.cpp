60c01abd57e404159ceb29b6fd13cb3a4b25f3b7
Valentin Reichenspurner <vreichenspurner@tum.de>
2025-09-02 11:17:48 +0200

    first macos working version + glad implementation

--- src/glfw-prototype/prototype.cpp
+++ src/glfw-prototype/prototype.cpp
@@ -1,6 +1,48 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
+#include <iostream>
+
+#include "linmath.h"
+
+
+typedef struct Vertex
+{
+	vec2 pos;
+	vec3 col;
+} Vertex;
+
+static const Vertex vertices[3] =
+{
+	{{-0.6f, -0.4f}, {1.f, 0.f, 0.f}},
+	{{ 0.6f, -0.4f}, {0.f, 1.f, 0.f}},
+	{{  0.f,  0.6f}, {0.f, 0.f, 1.f}}
+};
+
+
+static const char* vertex_shader_text = 
+"#version 330\n"
+"uniform mat4 MVP;\n"
+"in vec3 vCol;\n"
+"in vec2 vPos;\n"
+"out vec3 color;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+"	color = vCol;\n"
+"}\n";
+
+static const char* fragment_shader_text =
+"#version 330\n"
+"in vec3 color;\n"
+"out vec4 fragment;\n"
+"void main()\n"
+"{\n"
+"	fragment = vec4(color, 1.0);\n"
+"}\n";
+
 
 static void error_callback(int error, const char* description)
 {
@@ -21,7 +63,7 @@ int main()
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 
 	GLFWwindow* window = glfwCreateWindow(640, 480, "Vano-Prototype", NULL, NULL);
@@ -36,23 +78,66 @@ int main()
 
 
 	glfwMakeContextCurrent(window);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+	    std::cerr << "Failed to initialize GLAD" << std::endl;
+	    return -1;
+	}
+
+	glfwSwapInterval(1);
+	
+	GLuint vertex_buffer;
+	glGenBuffers(1, &vertex_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	const GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+	glCompileShader(vertex_shader);
+
+	const GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+	glCompileShader(fragment_shader);
+
+	const GLuint program = glCreateProgram();
+	glAttachShader(program, vertex_shader);
+	glAttachShader(program, fragment_shader);
+	glLinkProgram(program);
+
+	const GLint mvp_location = glGetUniformLocation(program, "MVP");
+	const GLint vpos_location = glGetAttribLocation(program, "vPos");
+	const GLint vcol_location = glGetAttribLocation(program, "vCol");
+
+	GLuint vertex_array;
+	glGenVertexArrays(1, &vertex_array);
+	glBindVertexArray(vertex_array);
+	glEnableVertexAttribArray(vpos_location);
+	glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*) offsetof(Vertex, pos));
+	glEnableVertexAttribArray(vcol_location);
+	glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*) offsetof(Vertex, col));
+
+
 	while (!glfwWindowShouldClose(window))
 	{
 		// Main Render loop
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+		const float ratio = width / (float) height;
 		
+		glViewport(0, 0, width, height);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// test if rendering works
-		glBegin(GL_TRIANGLES);
-		    glColor3f(1.0f, 0.0f, 0.0f); // red
-		    glVertex2f(-0.5f, -0.5f);
+		mat4x4 m, p, mvp;
+		mat4x4_identity(m);
+		mat4x4_rotate_Z(m, m, (float) glfwGetTime());
+		mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+		mat4x4_mul(mvp, p, m);
 
-		    glColor3f(0.0f, 1.0f, 0.0f); // green
-		    glVertex2f(0.5f, -0.5f);
+		glUseProgram(program);
+		glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) &mvp);
+		glBindVertexArray(vertex_array);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-		    glColor3f(0.0f, 0.0f, 1.0f); // blue
-		    glVertex2f(0.0f, 0.5f);
-		glEnd();
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
